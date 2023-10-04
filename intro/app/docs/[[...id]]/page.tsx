229c7b4450da4e70409cd0f4f6b1c411bb695fdc
victoriajquinto <victoriajquinto@gmail.com>
2023-10-04 14:21:19 -0700

    add styles to docs css module

--- intro/app/docs/[[...id]]/page.tsx
+++ intro/app/docs/[[...id]]/page.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 const DocsIdPage = ({ params }) => {
   console.log(params)
   return (
-    <div className={styles.title}>docs</div>
+    <div className={`${styles.title} ${styles.another}`}>docs</div>
   )
 }
 

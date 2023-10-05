d2c7caf00c6ee3aa56eb0c682afd300e2bc091a2
victoriajquinto <victoriajquinto@gmail.com>
2023-10-05 15:50:28 -0700

    add basic form with no content

---
+++ intro/components/NewToDoForm.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useState } from 'react'
+
+const NewToDoForm = () => {
+  const [ state, setState ] = useState()
+  return (
+    <div>
+        <form>
+          <input type='text'>
+
+          </input>
+        </form>
+    </div>
+  )
+}
+
+export default NewToDoForm
